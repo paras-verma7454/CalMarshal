295e6a77352350299164e3f623b30b84b28bf3e1
paras-verma7454 <parasverma7454@gmail.com>
2025-02-01 21:52:10 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "./components/ThemeProvider";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const geistSans = localFont({
